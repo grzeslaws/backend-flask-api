bf25e4849aa76909515b9ca9b71a8446326ac135
grzesieks <grzesieks@sparkbit.pl>
2018-06-19 09:41:34 +0200

    c3

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from app import app
 from app import settings
 
 if __name__ == "__main__":  
-    app.run(debug=settings.FLASK_DEBUG, port=settings.PORT)
+    app.run(debug=settings.FLASK_DEBUG, port=settings.PORT) 
